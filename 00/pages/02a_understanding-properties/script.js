441561093d8048f426dcc0722bd1e98451c4364e
Aditya Thorat <a.workspace.00@gmail.com>
2026-01-01 13:35:54 +0530

    changes

---
+++ 00/pages/02a_understanding-properties/script.js
@@ -0,0 +1,12 @@
+gsap.to(".box", {
+  opacity: 1,
+  rotation: 360,
+  background: "#ffff0ed4",
+  scale: 1.25,
+  duration: 2,
+  ease: "bounce",
+  repeat: -1,
+  yoyo: true,
+  repeatDelay: 2,
+  // paused: true,
+});
